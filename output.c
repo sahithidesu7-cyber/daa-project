aad2513ee4c8c2f1da567a539522b1c0918868c9
sahithidesu7-cyber <sahithidesu7@gmail.com>
2025-12-09 19:59:50 +0530

    Create output.c

---
+++ output.c
@@ -0,0 +1,126 @@
+ --- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 1
+Enter value: 50
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 1
+Enter value: 30
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 1
+Enter value: 70
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 1
+Enter value: 20
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 1
+Enter value: 40
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 1
+Enter value: 60
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 1
+Enter value: 80
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 4
+Inorder Traversal: 20 30 40 50 60 70 80 
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 2
+Enter value to search: 40
+Found!
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 2
+Enter value to search: 25
+Not Found!
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 3
+Enter value to delete: 20
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 3
+Enter value to delete: 70
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 4
+Inorder Traversal: 30 40 50 60 80 
+
+--- Binary Search Tree Menu ---
+1. Insert
+2. Search
+3. Delete
+4. Display (Inorder)
+5. Exit
+Enter choice: 5
